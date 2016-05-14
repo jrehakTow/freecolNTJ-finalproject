4db74f797808342bc8e0970cd2ac2b01853769bc
jrehakTow <jrehak1@students.towson.edu>
2016-05-14 12:39:32 -0400

    Fix issue 16
    Refactored av CC from 7 to 4

--- FreeCol/src/net/sf/freecol/server/generator/ColonizationMapLoader.java
+++ FreeCol/src/net/sf/freecol/server/generator/ColonizationMapLoader.java
@@ -32,6 +32,7 @@ import net.sf.freecol.common.model.Map.Layer;
 import net.sf.freecol.common.model.Specification;
 
 
+// TODO: Auto-generated Javadoc
 /**
  * Just pass the name of a Colonization map file (with extension ".MP").
  *
@@ -65,11 +66,19 @@ import net.sf.freecol.common.model.Specification;
  */
 public class ColonizationMapLoader implements MapLoader {
 
+    /** The Constant WIDTH. */
     public static final int WIDTH = 0;
+    
+    /** The Constant HEIGHT. */
     public static final int HEIGHT = 2;
+    
+    /** The Constant OCEAN. */
     public static final int OCEAN = 25;
+    
+    /** The Constant HIGH_SEAS. */
     public static final int HIGH_SEAS = 26;
 
+    /** The Constant tiletypes. */
     private static final String[] tiletypes = {
         "tundra",
         "desert",
@@ -101,11 +110,20 @@ public class ColonizationMapLoader implements MapLoader {
     };
 
 
+    /** The Constant header. */
     private static final byte[] header = {
         58, 0, 72, 0, 4, 0
     };
+    
+    /** The layer1. */
     private static byte[] layer1;
 
+    /**
+     * Instantiates a new colonization map loader.
+     *
+     * @param file the file
+     * @throws Exception the exception
+     */
     public ColonizationMapLoader(File file) throws Exception {
 
         RandomAccessFile reader = new RandomAccessFile(file, "r");
@@ -117,6 +135,9 @@ public class ColonizationMapLoader implements MapLoader {
 
     }
 
+    /* (non-Javadoc)
+     * @see net.sf.freecol.server.generator.MapLoader#loadMap(net.sf.freecol.common.model.Game, net.sf.freecol.common.model.Map.Layer)
+     */
     @Override
     public Layer loadMap(Game game, Layer layer) {
         Specification spec = game.getSpecification();
@@ -127,15 +148,7 @@ public class ColonizationMapLoader implements MapLoader {
         TileType tileType = null;
         if (highestLayer == Layer.LAND) {
             // import only the land / water distinction
-            for (int y = 0; y < header[HEIGHT]; y++) {
-                for (int x = 0; x < header[WIDTH]; x++) {
-                    int decimal = layer1[index] & 0xff;
-                    int terrain = decimal & 0b11111;
-                    tileType = (terrain == OCEAN || terrain == HIGH_SEAS) ?
-                        TileType.WATER : TileType.LAND;
-                    index++;
-                }
-            }
+            index = importLandWater(index);
         } else {
             TileImprovementType riverType = spec.getTileImprovementType("model.improvement.river");
             for (int y = 0; y < header[HEIGHT]; y++) {
@@ -144,23 +157,10 @@ public class ColonizationMapLoader implements MapLoader {
                     int terrain = decimal & 0b11111;
                     int overlay = decimal >> 5;
 
-                    if (terrain < tiletypes.length) {
-                        tileType = spec.getTileType("model.tile." + tiletypes[terrain]);
-                    } else if (overlay == 1 || overlay == 3) {
-                        tileType = spec.getTileType("model.tile.hills");
-                    } else if (overlay == 5 || overlay == 7) {
-                        tileType = spec.getTileType("model.tile.mountains");
-                    }
+                    tileType = checkTileTypes(spec, tileType, terrain, overlay);
                     tiles[x][y] = new Tile(game, tileType, x, y);
-                    if (highestLayer == Layer.RIVERS
-                        && (overlay == 2 || overlay == 3 || overlay == 6 || overlay == 7)) {
-                        TileItemContainer container = new TileItemContainer(game, tiles[x][y]);
-                        TileImprovement river =
-                            new TileImprovement(game, tiles[x][y], riverType);
-                        river.setMagnitude (overlay <= 3 ? 1 : 2);
-                        container.tryAddTileItem(river);
-                        tiles[x][y].setTileItemContainer(container);
-                    }
+                    checkRivers(game, tiles, highestLayer, riverType, y, x,
+							overlay);
                     index++;
                 }
             }
@@ -168,6 +168,74 @@ public class ColonizationMapLoader implements MapLoader {
         return highestLayer;
     }
 
+	/**
+	 * Check rivers.
+	 *
+	 * @param game the game
+	 * @param tiles the tiles
+	 * @param highestLayer the highest layer
+	 * @param riverType the river type
+	 * @param y the y
+	 * @param x the x
+	 * @param overlay the overlay
+	 */
+	private void checkRivers(Game game, Tile[][] tiles, Layer highestLayer,
+			TileImprovementType riverType, int y, int x, int overlay) {
+		if (highestLayer == Layer.RIVERS
+		    && (overlay == 2 || overlay == 3 || overlay == 6 || overlay == 7)) {
+		    TileItemContainer container = new TileItemContainer(game, tiles[x][y]);
+		    TileImprovement river =
+		        new TileImprovement(game, tiles[x][y], riverType);
+		    river.setMagnitude (overlay <= 3 ? 1 : 2);
+		    container.tryAddTileItem(river);
+		    tiles[x][y].setTileItemContainer(container);
+		}
+	}
+
+	/**
+	 * Check tile types.
+	 *
+	 * @param spec the spec
+	 * @param tileType the tile type
+	 * @param terrain the terrain
+	 * @param overlay the overlay
+	 * @return the tile type
+	 */
+	private TileType checkTileTypes(Specification spec, TileType tileType,
+			int terrain, int overlay) {
+		if (terrain < tiletypes.length) {
+		    tileType = spec.getTileType("model.tile." + tiletypes[terrain]);
+		} else if (overlay == 1 || overlay == 3) {
+		    tileType = spec.getTileType("model.tile.hills");
+		} else if (overlay == 5 || overlay == 7) {
+		    tileType = spec.getTileType("model.tile.mountains");
+		}
+		return tileType;
+	}
+
+	/**
+	 * Import land water.
+	 *
+	 * @param index the index
+	 * @return the int
+	 */
+	private int importLandWater(int index) {
+		TileType tileType;
+		for (int y = 0; y < header[HEIGHT]; y++) {
+		    for (int x = 0; x < header[WIDTH]; x++) {
+		        int decimal = layer1[index] & 0xff;
+		        int terrain = decimal & 0b11111;
+		        tileType = (terrain == OCEAN || terrain == HIGH_SEAS) ?
+		            TileType.WATER : TileType.LAND;
+		        index++;
+		    }
+		}
+		return index;
+	}
+
+    /* (non-Javadoc)
+     * @see net.sf.freecol.server.generator.MapLoader#getHighestLayer()
+     */
     @Override
     public Layer getHighestLayer() {
         return Layer.RIVERS;
