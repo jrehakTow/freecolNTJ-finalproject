5be420c464c81de30e0f66451db57c9f61676f25
Newsha <n.amiri@gmail.com>
2016-05-15 03:18:34 -0400

    Add test cases

--- FreeCol/test/src/net/sf/freecol/tools/ColonizationSaveGameReaderTest.java
+++ FreeCol/test/src/net/sf/freecol/tools/ColonizationSaveGameReaderTest.java
@@ -5,86 +5,96 @@ import static org.junit.Assert.*;
 
 /**
  * The class <code>ColonizationSaveGameReaderTest</code> contains tests for the class <code>{@link ColonizationSaveGameReader}</code>.
- *
- * @generatedBy CodePro at 5/10/16 11:07 AM
+ * @generatedBy CodePro at 5/14/16 8:10 PM
  * @author newsha
  * @version $Revision: 1.0 $
  */
 public class ColonizationSaveGameReaderTest {
 	/**
 	 * Run the ColonizationSaveGameReader(byte[]) constructor test.
-	 *
 	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
 	@Test
 	public void testColonizationSaveGameReader_1()
 		throws Exception {
 		byte[] data = new byte[] {};
-
 		ColonizationSaveGameReader result = new ColonizationSaveGameReader(data);
-
-		// add additional test code here
+		
 		assertNotNull(result);
 	}
 
 	/**
 	 * Run the String getString(byte[],int,int) method test.
-	 *
 	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
-	@Test
-	public void testGetString_1()
+	@Test(expected = java.lang.ArrayIndexOutOfBoundsException.class)
+	public void testGetString_1() 
 		throws Exception {
 		byte[] data = new byte[] {};
 		int start = 1;
-		int length = 1;
-
+		int length = 3;
 		String result = ColonizationSaveGameReader.getString(data, start, length);
-
-		// add additional test code here
-		// An unexpected exception was thrown in user code while executing this test:
-		//    java.lang.ArrayIndexOutOfBoundsException
-		//       at java.lang.System.arraycopy(Native Method)
-		//       at java.util.Arrays.copyOfRange(Arrays.java:3521)
-		//       at net.sf.freecol.tools.ColonizationSaveGameReader.getString(ColonizationSaveGameReader.java:223)
+		
 		assertNotNull(result);
 	}
 
 	/**
 	 * Run the String getString(byte[],int,int) method test.
-	 *
 	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
-	@Test
+	@Test(expected = java.lang.ArrayIndexOutOfBoundsException.class)
 	public void testGetString_2()
 		throws Exception {
 		byte[] data = new byte[] {};
-		int start = 1;
-		int length = 1;
+		int start = -1 ;
+		int length = 3;
 
 		String result = ColonizationSaveGameReader.getString(data, start, length);
 
-		// add additional test code here
-		// An unexpected exception was thrown in user code while executing this test:
-		//    java.lang.ArrayIndexOutOfBoundsException
-		//       at java.lang.System.arraycopy(Native Method)
-		//       at java.util.Arrays.copyOfRange(Arrays.java:3521)
-		//       at net.sf.freecol.tools.ColonizationSaveGameReader.getString(ColonizationSaveGameReader.java:223)
+		assertNotNull(result);
+	}
+	
+	/**
+	 * Run the String getString(byte[],int,int) method test.
+	 * @throws Exception
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
+	 */
+	@Test(expected = java.lang.ArrayIndexOutOfBoundsException.class)
+	public void testGetString_3()
+		throws Exception {
+		byte[] data = new byte[] {};
+		int start = 0 ;
+		int length = 0;
+
+		String result = ColonizationSaveGameReader.getString(data, start, length);
+
+		assertNotNull(result);
+	}
+	
+	/**
+	 * Run the String getString(byte[],int,int) method test.
+	 * @throws Exception
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
+	 */
+	@Test(expected = java.lang.ArrayIndexOutOfBoundsException.class)
+	public void testGetString_4()
+		throws Exception {
+		byte[] data = new byte[] {};
+		int start = -3 ;
+		int length = -1;
+
+		String result = ColonizationSaveGameReader.getString(data, start, length);
+
 		assertNotNull(result);
 	}
 
 	/**
 	 * Run the void main(String[]) method test.
-	 *
 	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
 	@Test(expected = java.lang.ArrayIndexOutOfBoundsException.class)
 	public void testMain_1()
@@ -93,15 +103,12 @@ public class ColonizationSaveGameReaderTest {
 
 		ColonizationSaveGameReader.main(args);
 
-		// add additional test code here
 	}
 
 	/**
 	 * Run the void main(String[]) method test.
-	 *
 	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
 	@Test(expected = java.lang.ArrayIndexOutOfBoundsException.class)
 	public void testMain_2()
@@ -115,10 +122,8 @@ public class ColonizationSaveGameReaderTest {
 
 	/**
 	 * Run the void main(String[]) method test.
-	 *
 	 * @throws Exception
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
 	@Test(expected = java.lang.ArrayIndexOutOfBoundsException.class)
 	public void testMain_3()
@@ -136,7 +141,7 @@ public class ColonizationSaveGameReaderTest {
 	 * @throws Exception
 	 *         if the initialization fails for some reason
 	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
 	@Before
 	public void setUp()
@@ -146,11 +151,9 @@ public class ColonizationSaveGameReaderTest {
 
 	/**
 	 * Perform post-test clean-up.
-	 *
 	 * @throws Exception
 	 *         if the clean-up fails for some reason
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
 	@After
 	public void tearDown()
@@ -160,10 +163,8 @@ public class ColonizationSaveGameReaderTest {
 
 	/**
 	 * Launch the test.
-	 *
 	 * @param args the command line arguments
-	 *
-	 * @generatedBy CodePro at 5/10/16 11:07 AM
+	 * @generatedBy CodePro at 5/14/16 8:10 PM
 	 */
 	public static void main(String[] args) {
 		new org.junit.runner.JUnitCore().run(ColonizationSaveGameReaderTest.class);
